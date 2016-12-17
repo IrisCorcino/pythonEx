d2ad80e7dbc54168d0b4bd3bc97d0478de837b0b
IrisCorcino <iris_oliveira@live.com>
2016-12-17 12:43:23 +0100

    added test1

---
+++ test1.py
@@ -0,0 +1,17 @@
+intervalo = int( input( "Intervalo = "))
+cont = 1
+quant = 0
+while  cont <= intervalo :
+    cont2 = 1
+    soma = 0
+    while cont2 < cont:
+        resto = cont % cont2
+        if resto == 0:
+            soma += cont2
+        cont2 += 1
+    if soma == cont:
+        quant += 1
+        print cont
+    cont += 1
+
+print "Amont of perfect number are %i " % quant
